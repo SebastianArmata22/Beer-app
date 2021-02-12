1a03b7bf5011e12a381507e0c40fee0176c726b3
SebastianArmata <62599102+SebastianArmata22@users.noreply.github.com>
2021-02-12 23:44:51 +0100

    bugs fixed

---
+++ .history/src/components/App_20210212233708.js
@@ -0,0 +1,58 @@
+import './App.css';
+import Card from './Card.js'
+import React from 'react'
+import rigtArrow from'../icons/right-arrow.svg'
+import leftArrow from'../icons/left-arrow.svg'
+
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      isLoaded:false,
+      data:[],
+      isNext:true
+    }
+    this.counter=1
+    this.nextBeer=this.nextBeer.bind(this)
+    this.previousBeer=this.previousBeer.bind(this)
+  }
+  getUrl(){
+   return `https://api.punkapi.com/v2/beers/${this.counter}`
+  }
+  getDateFromApi(url){
+    fetch(url)
+    .then(response=>response.json())
+    .then(dataApi=>this.setState({
+      isLoaded: true,
+      data: dataApi}))
+  }
+  nextBeer(){
+    this.setState({isLoaded: false})
+    this.counter<325 && this.counter++
+    this.getDateFromApi(this.getUrl())
+    this.setState({isNext:true})
+  }
+ previousBeer(){
+    this.setState({isLoaded: false})
+    this.counter>1 && this.counter--
+    this.getDateFromApi(this.getUrl());
+    this.setState({isNext:false})
+  }
+  componentDidMount(){
+    this.getDateFromApi(this.getUrl());
+  }
+  render(){
+    return(
+        <div className="app-container">
+        <img className="svg-button" onClick={this.previousBeer} src={leftArrow} alt="previous" height='100px'/>
+        {this.state.isLoaded ? <Card item={this.state.data} isNext={this.state.isNext} /> : <div className=" load-div"><h3>is Loading...</h3></div>}
+        <img className="svg-button" onClick={this.nextBeer} src={rigtArrow} alt="next" height='100px' />
+        </div>
+
+    )
+  }
+}
+
+export default App;
